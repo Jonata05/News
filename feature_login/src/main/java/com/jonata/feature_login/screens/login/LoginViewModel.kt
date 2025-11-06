48a8c10d6987af72a64b20f2282fa48bfdd66899
Jonata Ferreira <jntferreiratj@hotmail.com>
2025-11-06 17:37:27 -0300

    Adiciona módulo feature_login (código-fonte apenas)

---
+++ feature_login/src/main/java/com/jonata/feature_login/screens/login/LoginViewModel.kt
@@ -0,0 +1,80 @@
+package com.jonata.feature_login.screens.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jonata.core.navigation.NavRoute
+import com.jonata.domain.model.UserCredentials
+import com.jonata.domain.model.Email
+import com.jonata.domain.model.Password
+import com.jonata.domain.generics.Validate
+import com.jonata.domain.usecase.login.LoginUseCase
+import com.jonata.domain.usecase.generics.UseCaseExecutor
+import com.jonata.feature_login.navigation.NewsNavGraphRoute
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase,
+    private val useCaseExecutor: UseCaseExecutor
+) : ViewModel() {
+
+    private val _navigate = MutableSharedFlow<NavRoute>()
+    val navigate = _navigate.asSharedFlow()
+
+    private val _email = MutableStateFlow("")
+    val email: StateFlow<String> = _email
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password
+
+
+
+    fun updateEmail(value: String){
+        _email.value = value
+    }
+
+    fun updatePassword(value: String){
+        _password.value = value
+    }
+
+
+    fun sendCredentials(){
+        Validate {
+            UserCredentials(
+                email = Email(email.value),
+                password = Password(password.value)
+            )
+        }.isSuccess { login(it) }.catchException { e-> }
+    }
+
+    fun login(userCredentials: UserCredentials){
+
+
+            useCaseExecutor(loginUseCase, userCredentials) {
+
+                isSuccess {
+
+                    viewModelScope.launch {
+                        _navigate.emit( NewsNavGraphRoute.NewsHomeScreen)
+                    }
+
+                }
+
+                isFailure {
+
+                }
+
+            }
+
+
+    }
+
+
+}
