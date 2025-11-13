fad2b2e36ece866c99b3375a643700bbf5db9a28
Jonata Ferreira <jntferreiratj@hotmail.com>
2025-11-13 09:50:36 -0300

    primeiro commit

--- app/src/main/java/com/jonata/news/application/NewsAndroidApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.jonata.news.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NewsAndroidApplication: Application() {
-}
-
